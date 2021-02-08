315f76d327ae21c092745666c6910abc2acdb812
Diogo Condêço <diogobcondeco@gmail.com>
2021-02-08 21:26:19 +0000

    Design completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,55 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container content-box">
+                <h1 className="title">Hello, I'm Diogo!</h1>
+                <h2 className="subtitle">
+                    A Front End Developer that loves to<span className="whiteSpace"> </span>
+                    <a href="https://github.com/diogobcondeco" target="_blank" rel="noreferrer">
+                        <span className="content">build</span>
+                    </a><span className="whiteSpace"> </span>things.
+                </h2>
+                <p className="paragraph">
+                    Passionate about<span className="whiteSpace"> </span>
+                    <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                        <span className="content">React</span>
+                    </a><span className="whiteSpace"> </span>&<span className="whiteSpace"> </span>
+                    <a href="https://vuejs.org/" target="_blank" rel="noreferrer">
+                        <span className="content">Vue</span>
+                    </a>.
+                </p>
+                <p className="paragraph">
+                    Goals of mastering<span className="whiteSpace"> </span>
+                    <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                        <span className="content">React</span>
+                    </a><span className="whiteSpace"> </span>& contributing to it.
+                </p>
+                <p className="paragraph">
+                    Dreaming about traveling the 🌍 while 💻 remotely.
+                </p>
+                <p className="paragraph">
+                    Lover of 🍔🍕🍣✈🚐.
+                </p>
+                <p className="paragraph">
+                    You can follow on<span className="whiteSpace"> </span>
+                    <a href="https://twitter.com/diogobcondeco" target="_blank" rel="noreferrer">
+                        <span className="content">Twitter</span>
+                    </a>,<span className="whiteSpace"> </span>
+                    <a href="https://github.com/diogobcondeco" target="_blank" rel="noreferrer">
+                        <span className="content">GitHub</span>
+                    </a><span className="whiteSpace"> </span>&<span className="whiteSpace"> </span>
+                    <a href="https://www.linkedin.com/in/diogobcondeco/" target="_blank" rel="noreferrer">
+                        <span className="content">LinkedIn</span>
+                    </a>.
+                </p>
+                <p className="paragraph">
+                    <a href="mailto:diogobcondeco@gmail.com">↳ Talk to me.</a>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
