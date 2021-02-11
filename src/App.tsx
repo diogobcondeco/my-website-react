1f02803f4652184cf4c77e9fa691d627d713fd4e
Diogo Condêço <diogobcondeco@gmail.com>
2021-02-11 00:25:47 +0000

    Changed learning languages paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
                     Speaker of english, português & español.
                 </p>
                 <p className="paragraph">
-                    Hopefully 한국어, 日本人, 中文, français & italiano soon.
+                    Learning 한국어 & hopefully more soon.
                 </p>
                 <p className="paragraph">
                     You can follow on<span className="whiteSpace"> </span>
